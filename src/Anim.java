4dee46bf8069e91fb79e27478447b2c0c3a5aad1
AhmedEmadeldinMohamed <ahmedemadd534@gmail.com>
2025-12-09 21:39:16 +0200

    The main

---
+++ src/Anim.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Anim extends JFrame implements ActionListener {
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}

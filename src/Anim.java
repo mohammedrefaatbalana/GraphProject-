c0a56dcc09e2ebd6a753cc0c33c8309101edba2d
mohammedrefaatbalana <mohammed.refaat.balana@gmail.com>
2025-12-10 08:53:07 +0200

    Implement Nimbus look and feel in Anim class
    Set Nimbus look and feel for the application.

--- src/Anim.java
+++ src/Anim.java
@@ -9,6 +9,18 @@ import java.util.*;
 
 public class Anim extends JFrame implements ActionListener {
     public static void main(String[] args) {
+
+
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Anim.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
     
     }
 
